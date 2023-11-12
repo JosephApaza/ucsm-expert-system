f7e38892649e6df05bacf5c625cda39bbda3c5a8
Joseph Apaza <joseph.apaza@ucsm.edu.pe>
2023-11-12 18:47:31 -0500

    Corrección: Cambio de campo ID a autoincremental.

--- sql/create_table_pasos.sql
+++ sql/create_table_pasos.sql
@@ -1,8 +1,8 @@
--- Este script crea la tabla de Pasos
+-- Este script crea la tabla de Pasos con ID como una columna de identidad
 USE GestionTramitesDB;
 
 CREATE TABLE Pasos (
-    ID INT PRIMARY KEY,
+    ID INT IDENTITY(1,1) PRIMARY KEY,
     ID_Proceso INT,
     Orden INT,
     Descripcion NVARCHAR(MAX),
