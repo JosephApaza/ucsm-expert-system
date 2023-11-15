490402c9ded8c38b69da1e82a6207a48a5e22ab9
Joseph Apaza <joseph.apaza@ucsm.edu.pe>
2023-11-15 16:04:12 -0500

    Creación de validaciones de proceso existente (ID existe).

--- controllers/pasosController.py
+++ controllers/pasosController.py
@@ -1,16 +1,34 @@
 from config import app, db
-from flask import render_template
+from flask import render_template, abort
 from sqlalchemy import text
 
 @app.route("/proceso/<id_proceso>", methods=['POST', 'GET'])
 def mostrar_pasos(id_proceso):
-    # Simplemente seleccionamos el primer proceso por ahora
-    pasos_proceso = db.session.execute(text('EXEC ObtenerPasosDelProceso :id_proceso'), {'id_proceso': id_proceso})
+    try:
+        # Intentar convertir id_proceso a un entero
+        id_proceso = int(id_proceso)
+    except ValueError:
+        # Si la conversión falla, retornar un error 400 (Bad Request)
+        abort(400, "ID de proceso no válido. Debe ser un número entero.")
+
+    # Obtener la lista de pasos del proceso desde la base de datos
+    pasos_proceso = db.session.execute(
+        text('EXEC ObtenerPasosDelProceso :id_proceso'), {'id_proceso': id_proceso}
+    )
+
+    # Verificar si el resultado está vacío, lo que indica que el proceso no existe
+    if not pasos_proceso.rowcount:
+        # Si el proceso no existe, retornar un error 404 (Not Found)
+        abort(404, f"No se encontró el proceso con ID {id_proceso}.")
+
+    # Obtener los pasos del resultado de la consulta
     pasos = [{'Orden': row.Orden, 'Descripcion': row.Descripcion} for row in pasos_proceso]
-    
-    nombre_proceso = db.session.execute(text('EXEC ObtenerNombrePorID :id_proceso'), {'id_proceso': id_proceso})
+
+    # Obtener el nombre del proceso
+    nombre_proceso = db.session.execute(
+        text('EXEC ObtenerNombrePorID :id_proceso'), {'id_proceso': id_proceso}
+    )
     nombre = [{'Nombre': row.Nombre} for row in nombre_proceso]
 
-    
-    # Renderizamos la plantilla y pasamos los datos a la misma
+    # Renderizar la plantilla y pasar los datos
     return render_template("pasos.html", pasos=pasos, nombre=nombre)
